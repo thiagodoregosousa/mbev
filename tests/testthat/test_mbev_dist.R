7db865d56425233b8b452da0544c32dffbbf589e
Thiago <thiago@thiagos-MacBook-Pro.local>
2026-01-17 11:58:56 -0300

    added edge cases for rmbev

--- tests/testthat/test_mbev_dist.R
+++ tests/testthat/test_mbev_dist.R
@@ -22,6 +22,7 @@ test_that("Passing invalid to my function", {
   
 })
 
+
 test_that("Output format", {
   
   
@@ -34,17 +35,36 @@ test_that("Output format", {
 })
 
 
-
-
 test_that("Checking correct behaviour for knonw values", {
   
   # set.seed fixes generated value
   set.seed(1)
   xy = rmbev(n = 2)
   expect_equal(as.vector(xy[2,2]), 2.788448, tolerance = 0.0001)
+})
+
+
+test_that("Edge cases behaviour", {
   
+  # xi close to zero gives valid values
+  expect_silent(rmbev(xi1 = 1e-30))
+  
+  # Inf values appear when xi is big
+  x1 = rmbev(n = 10, xi1 = 1e30)[,1]
+  expect_gt(sum(x1 == Inf), 1)
+  x2 = rmbev(n = 10, xi2 = 1e30)[,2]
+  expect_gt(sum(x2 == Inf), 1)
+  x = rmbev(n = 10, xi1 = 1e30, xi2 = 1e30)
+  expect_gt(sum(x[,1] == Inf), 1)
+  expect_gt(sum(x[,2] == Inf), 1)
+  
+  # abs(Observations) get close to 1 if delta is big 
+  expect_equal(abs(as.vector(rmbev(n = 10, delta1 = 100000000)[10,1])), 1, tolerance = 0.0001)
   
 })
+
+
+
 
 
 
